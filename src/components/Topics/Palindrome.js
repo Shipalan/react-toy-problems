56eafbc2282a3202fac46be4ad2a5d707c20387c
Shipalan <shipalan678@gmail.com>
2022-07-28 17:40:32 -0600

    Finished palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
 
 const Palindrome = () => {
-    return <p>Palindrome</p>
-}
+  const [userInput, setUserInput] = useState("");
+  const [palindrome, setPalindrome] = useState("");
+
+  const checker = (v) => {
+    let str = v.split("").reverse().join("");
+    if (str.toLowerCase() === v.toLowerCase()) {
+      setPalindrome("true");
+    } else {
+      setPalindrome("false");
+    }
+  };
+
+  return (
+    <div className="puzzleBox palindromePB">
+      <h4>Palindrome</h4>
+      <input
+        className="inputLine"
+        onChange={(e) => setUserInput(e.target.value)}
+      ></input>
+      <button className="confirmationButton" onClick={() => checker(userInput)}>
+        Confirm
+      </button>
+      <span className="resultsBox">Is Palindrome: {palindrome}</span>
+    </div>
+  );
+};
 
 export default Palindrome;
